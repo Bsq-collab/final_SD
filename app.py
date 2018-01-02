9dafc212bd0d589e9cbab71986591ee848f6a2d5
vivienlee00 <vivien.lee.601@gmail.com>
2018-01-02 12:07:06 -0500

    app.py routes to some templates

--- app.py
+++ app.py
@@ -6,11 +6,19 @@ app= Flask(__name__)
 #default homepage
 @app.route("/")
 def welcome():
-    return ""
+    return render_template("home.html")
 
 @app.route("/view")
 def tablify():
     return ""
+
+@app.route("/directions")
+def instructions():
+    return render_template("directions.html")
+
+@app.route("/start")
+def start():
+    return render_template("playerSignUp.html")
 
 
 if __name__== "__main__":

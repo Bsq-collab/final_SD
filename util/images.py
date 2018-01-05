ec05f87a3438da87daf95cedb451146acaaa8b35
Charles <cweng@stuy.edu>
2018-01-05 13:01:37 -0500

    getty key fix

--- util/images.py
+++ util/images.py
@@ -2,29 +2,35 @@ import urllib2
 import urllib
 import json
 
+
 def getKey():
     try:
         f = open("gettykeys.txt")
         key = f.read()
-        return key.strip() #Removes Whitespace
+        print key
+        return key.strip()  # Removes Whitespace
     except Exception as e:
+        print e
         print "****API KEY NOT FOUND******"
         return ""
 
-def image(answer): #input the description of the desired image, which is the answer to the jeopardy question
+
+def image(answer):  # input the description of the desired image, which is the answer to the jeopardy question
     answer = urllib.quote(answer)
     key = getKey()
-    #print key
+    # print key
     if key != "":
-        url = urllib2.Request("https://api.gettyimages.com/v3/search/images?sort_order=most_popular&phrase=" + answer, headers={ 'Api-Key' : getKey()})
+        url = urllib2.Request("https://api.gettyimages.com/v3/search/images?sort_order=most_popular&phrase=" + answer, headers={'Api-Key': getKey()})
     else:
         return key
     print url
-    uResp = urllib2.urlopen( url )
+    uResp = urllib2.urlopen(url)
     contentsraw = uResp.read()
     dat = json.loads(contentsraw)
     if len(dat['images']) == 0:
         return "https://rlv.zcache.com/sad_smiley_face_classic_round_sticker-r364e0eed23d248b982dc0b717710afc1_v9wth_8byvr_324.jpg"
     else:
-        return dat['images'][0]['display_sizes'][0]['uri'] #outputs a url to the image
+        return dat['images'][0]['display_sizes'][0]['uri']  # outputs a url to the image
+
+
 print image("juice")

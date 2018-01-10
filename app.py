52b22fff59a4c586a8eb1fc4db13f73ef81f99e0
Bayan Berri <bberri@stuy.edu>
2018-01-10 13:20:38 -0500

    worked on game()

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+from util import api
 
 
 app = Flask(__name__)
@@ -22,17 +23,27 @@ def start():
 
 @app.route("/game", methods=["POST","GET"])
 def game():
-    # pass round data
+    cons= ['b','c','d','f','g','h','j','k','l','m','n','p','q','r','s','t','v','w','x','y','z']
+    vow=['a','e','i','o','u']
+    dictionary=api.jeopardy()
+    ans=dictionary['answer']
     # bar at the top w player #s + money
     # letter board: while not full, continue cycling through players guessing letters
     # when full, the game is over -> displays name of winner + $ 
     # spinning the wheel is just an html table changing colors
     # basic player turn example: guess phrase/buy a vowel/spin -> displays whether the letter was right or not (refresh board) -> end turn
-    # letters guessed are stored in an array
-    # cpus guess random letters
+    
+    random_letter(cons) #cpu guesses
     return render_template("game.html")
 
+def random_letter(letters):
+    x= random.randint(0,letters.length)
+    return letters.pop(x)
+    
 
+def make_blanks(string):
+    blank=""
+    
 if __name__ == "__main__":
     app.debug = True
     app.run()

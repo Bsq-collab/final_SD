0737f3601bc6606fce24329ed856653f75af8369
Charles <cweng@stuy.edu>
2018-01-11 09:32:09 -0500

    js

---
+++ static/script.js
@@ -0,0 +1,89 @@
+// defined for game
+const cons = ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'];
+const vow = ['a', 'e', 'i', 'o', 'u'];
+
+// keeps track of current game's vowels and consts
+var tempc = cons;
+var tempv = vow;
+
+// get the answer title and hint
+var answer = document.getElementById("answer");
+var title = document.getElementById("title");
+var hint = document.getElementById("hint");
+
+var ans = answer.innerHTML;
+var tit = title.innerHTML;
+var hin = hint.innerHTML;
+
+answer.remove();
+title.remove();
+hint.remove();
+
+console.log(ans);
+console.log(tit);
+console.log(hin);
+
+// process ans into 2d array of characters (ans[]->word; ans[][]->char)
+ans = ans.split(" ");
+for (var i = 0; i < ans.length; i++){
+  ans[i] = ans[i].split("");
+  console.log(ans[i]);
+}
+
+// create the display
+var body = document.getElementById('body');
+
+// insert title
+var category = document.createElement('div');
+category.className = "category";
+category.innerHTML = "Category: " + tit + "\n";
+
+// create game board
+// loop first by word
+for (var i = 0; i < ans.length; i++){
+  var el = document.createElement("ul");
+  el.className = "inlineList";
+  el.innerHTML += "\n"
+  // list out the the characters of the word
+  for (var o = 0; o < ans[i].length; o++){
+    var ell = document.createElement("li");
+    // what the letter is supposed to be will be stored in the elemet name
+    ell.setAttribute("name", ans[i][o]);
+    ell.className = "char";
+    // check if the character needs to be hidden
+    if(cons.indexOf(ans[i][o] >= 0 || vow.indexOf(ans[i][o] >= 0)))
+      ell.innerHTML = "_";
+    else
+      ell.innerHTML = ans[i][0];
+
+    el.appendChild(ell);
+    el.innerHTML += "\n"
+  }
+  category.appendChild(el);
+  category.innerHTML += "\n"
+}
+
+var chars = category.getElementsByTagName("li");
+body.appendChild(category);
+body.innerHTML += "\n"
+console.log(chars)
+
+// buttons
+var buttons = document.getElementById("buttons");
+
+var popups = buttons.getElementsByClassName("popup")
+console.log(popups[0]);
+
+var guess = document.createElement("button");
+guess.onclick = function(){
+  popups[0].style.display = "block";
+}
+guess.innerHTML = "Guess a Constanent"
+buttons.appendChild(guess);
+
+buttons.innerHTML += "\n"
+
+console.log(body);
+console.log(body.innerHTML);
+console.log(buttons);
+console.log(buttons.innerHTML);
